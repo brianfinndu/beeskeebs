b19213d358f5edb58fa1efea358a0d0050516f5b
Brian Du <brian.finn.du@gmail.com>
2024-04-07 19:45:00 -0700

    Major changes pushed - future changes will be more atomic.

--- scripts.js
+++ scripts.js
@@ -1,94 +1,129 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
+// TO DO: adjust opacity for active elements
+// TO DO: "edit keeb" interface
+// TO DO: "add keeb" interface
+// TO DO: "remove keeb" interface (with Alert warning "lost to the ether...")
+// TO DO: adjust background colors and/or build name based on active index
 
+let activeIndex = 0;
+let activeSubIndex = 0;
+let photoIndex = 0;
+let mainContent = [];
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+// This calls the addCards() function when the page is first loaded
+document.addEventListener("DOMContentLoaded", loadContent);
 
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
-];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
+async function loadContent() {
+    const leftArrow = document.getElementById("left-arr")
+    leftArrow.addEventListener("click", () => {
+        activeSubIndex--;
+        activeSubIndex = (activeSubIndex + mainContent[activeIndex]["media-urls"].length) % mainContent[activeIndex]["media-urls"].length;
+        updateFocusImg();
+    })
 
-
-// This function adds cards the page to display the data in the array
-function showCards() {
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = "";
-    const templateCard = document.querySelector(".card");
+    const rightArrow = document.getElementById("right-arr")
+    rightArrow.addEventListener("click", () => {
+        activeSubIndex++;
+        activeSubIndex = activeSubIndex % mainContent[activeIndex]["media-urls"].length;
+        updateFocusImg();
+    })
     
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
+    let req = new XMLHttpRequest();
 
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
+    req.onload = () => 
+    {
+        mainContent = JSON.parse(req.response);
+        
+        populateLeft();
+        populateCenterLow();
+        updateFocusImg();
+        updateRight();
+    }
 
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
+    req.open("GET", "https://api.jsonbin.io/v3/b/6611d64ce41b4d34e4e0661e/latest");
+    req.setRequestHeader("X-Master-Key", "$2b$10$NcpW2rFP851TgVn6DZ4iQu/akYysLsu/UC8B7COjY2WiWzEmsAg4y");
+    req.setRequestHeader("X-Bin-Meta", "false");
+    req.send();
+}
+
+function populateLeft() {
+    const leftDiv = document.getElementById("left");
+    leftDiv.innerHTML = "";
+    for(let i = 0; i < mainContent.length; i++)
+    {
+        let new_img = document.createElement("img");
+        new_img.className = "left-icon";
+        new_img.src = mainContent[i]["media-urls"][0];
+        new_img.setAttribute("item-index", i);
+        new_img.addEventListener("click", (event) => {
+            activeIndex = event.target.getAttribute("item-index");
+            activeSubIndex = 0;
+            updateCenter();
+            updateRight();
+        })
+        leftDiv.appendChild(new_img);
     }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
-    card.style.display = "block";
-
-    const cardHeader = card.querySelector("h2");
-    cardHeader.textContent = newTitle;
-
-    const cardImage = card.querySelector("img");
-    cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
-
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
+function populateCenterLow() {
+    const centerLow = document.getElementById("center-low");
+    centerLow.innerHTML = "";
+    for(let i = 0; i < mainContent[activeIndex]["media-urls"].length; i++)
+    {
+        let new_img = document.createElement("img");
+        new_img.className = "center-icon";
+        new_img.src = mainContent[activeIndex]["media-urls"][i];
+        new_img.setAttribute("item-index", i);
+        new_img.addEventListener("click", (event) => {
+            activeSubIndex = event.target.getAttribute("item-index");
+            updateFocusImg();
+        })
+        centerLow.appendChild(new_img);
+    }
 }
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
+function updateFocusImg() {
+    const focusImage = document.getElementById("focus-img");
+    focusImage.src = mainContent[activeIndex]["media-urls"][activeSubIndex];
 }
 
-function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
+function updateCenter() {
+    populateCenterLow();
+    updateFocusImg();
+}
+
+function updateRight() {
+    const rightDiv = document.getElementById("right");
+    rightDiv.innerHTML = "";
+
+    let new_h1 = document.createElement("h1");
+    new_h1.innerHTML = mainContent[activeIndex]["name"];
+    rightDiv.appendChild(new_h1);
+
+    for(const attr in mainContent[activeIndex]["build-specs"])
+    {
+        let new_p = document.createElement("p");
+        new_p.innerHTML = attr + ":";
+        rightDiv.appendChild(new_p);
+        
+        let new_ul = document.createElement("ul");
+
+        if (attr != "Description")
+        {
+            for(const nestedAttr in mainContent[activeIndex]["build-specs"][attr])
+            {
+                let new_li = document.createElement("li");
+                new_li.innerHTML = nestedAttr + ": " + mainContent[activeIndex]["build-specs"][attr][nestedAttr];
+                new_ul.appendChild(new_li);
+            }
+        }
+
+        else
+        {
+            let new_li = document.createElement("li");
+            new_li.innerHTML = mainContent[0]["build-specs"][attr];
+            new_ul.appendChild(new_li);
+        }
+
+        rightDiv.appendChild(new_ul);
+    }
 }
